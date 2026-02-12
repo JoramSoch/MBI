8e215cba12d117b4052d3463f605899802f96ef7
Joram Soch <joram.soch@bccn-berlin.de>
2026-02-12 17:22:16 +0100

    rewrote Analysis 3
    Analysis 3, birth weight data, was rewritten to (i) allow for SVM analyses without external functions, (ii) allow for weighting samples by class size and (iii) allow for using covariates as features. The SVM analysis to plot can be selected, the original analysis being the default.

--- Examples/Analysis_3/Analysis_3.m
+++ Examples/Analysis_3/Analysis_3.m
@@ -9,6 +9,9 @@
 % - 30/05/2022, 05:33: minor changes
 % - 27/02/2025, 11:27: aligned with Python
 % - 11/02/2026, 16:59: recorded analysis time
+% - 12/02/2026, 17:16: rewrote SVM analysis code,
+%                      included weight samples by class size,
+%                      included use covariates as features
 
 
 clear
@@ -38,50 +41,108 @@ v = size(Y,2);                  % number of features
 %%% Step 2: analyze data %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % specify cross-validation
-k  = 10;                        % number of CV folds
-V  = eye(n);                    % observation covariance
-tA = 0;
-tB = 0;
+SVMs={'original', 'features', 'weighted'};
+Stp = 'original';               % SVM to plot
+C   = 1;                        % SVM cost parameter
+k   = 10;                       % number of CV folds
+V   = eye(n);                   % observation covariance
+tA  = 0;
+tB  = 0;
 
 % Analysis 1: classify smoker, (not) accounting for others
 x1  = X(:,1);
-% X1  = [1*(X(:,2)==1)-1*(X(:,2)>1), 1*(X(:,3)==1)-1*(X(:,3)==2), X(:,4:5)];
 X1  = [1*(X(:,2)==1)-1*(X(:,2)==2), 1*(X(:,2)==2)-1*(X(:,2)==3), 1*(X(:,3)==1)-1*(X(:,3)==2), X(:,4:5)];
-% X1  = [1*(X(:,2)==1)-1*(X(:,2)==2), 1*(X(:,2)==2)-1*(X(:,2)==3), 1*(X(:,3)==1)-1*(X(:,3)==2)];
-% X1  = [X(:,4:5)];
 CV1 = ML_CV(x1, k, 'kfc');
 X1r = [X1, ones(n,1)];
 Y1r = (eye(n) - X1r*(X1r'*X1r)^(-1)*X1r')*Y;
 tic; MBC(1) = ML_MBI(Y, x1, X1, V, CV1, 'MBC', []); tA = tA + toc;
-tic; SVC(1) = ML_SVC(x1, Y1r, CV1, 1, 1, 0);        tB = tB + toc;
 
 % Analysis 2: classify ethnicity, (not) accounting for others
 x2  = X(:,2);
 X2  = [1*(X(:,1)==1)-1*(X(:,1)==2), 1*(X(:,3)==1)-1*(X(:,3)==2), X(:,4:5)];
-% X2  = [1*(X(:,1)==1)-1*(X(:,1)==2), 1*(X(:,3)==1)-1*(X(:,3)==2)];
-% X2  = [X(:,4:5)];
 CV2 = ML_CV(x2, k, 'kfc');
 X2r = [X2, ones(n,1)];
 Y2r = (eye(n) - X2r*(X2r'*X2r)^(-1)*X2r')*Y;
 tic; MBC(2) = ML_MBI(Y, x2, X2, V, CV2, 'MBC', []); tA = tA + toc;
-tic; SVC(2) = ML_SVC(x2, Y2r, CV2, 1, 1, 0);        tB = tB + toc;
 
 % Analysis 3: classify hypertension, (not) accounting for others
 x3  = X(:,3);
-% X3  = [1*(X(:,1)==1)-1*(X(:,1)==2), 1*(X(:,2)==1)-1*(X(:,2)>1), X(:,4:5)];
 X3  = [1*(X(:,1)==1)-1*(X(:,1)==2), 1*(X(:,2)==1)-1*(X(:,2)==2), 1*(X(:,2)==2)-1*(X(:,2)==3), X(:,4:5)];
-% X3  = [1*(X(:,1)==1)-1*(X(:,1)==2), 1*(X(:,2)==1)-1*(X(:,2)==2), 1*(X(:,2)==2)-1*(X(:,2)==3)];
-% X3  = [X(:,4:5)];
 CV3 = ML_CV(x3, 6, 'kfc');
 X3r = [X3, ones(n,1)];
 Y3r = (eye(n) - X3r*(X3r'*X3r)^(-1)*X3r')*Y;
 tic; MBC(3) = ML_MBI(Y, x3, X3, V, CV3, 'MBC', []); tA = tA + toc;
-tic; SVC(3) = ML_SVC(x3, Y3r, CV3, 1, 1, 0);        tB = tB + toc;
+
+% Analyses 1-3: support vector classifications
+p   = find(strcmp(SVMs,Stp));
+xs  = {x1,  x2,  x3 };
+Ys  = {Y,   Y,   Y  };
+Xs  = {X1,  X2,  X3 };
+Yr  = {Y1r, Y2r, Y3r};
+CV  = {CV1, CV2, CV3};
+
+% analyze data sets
+for h = 1:numel(Yr)
+
+    % get label values
+    xt = xs{h};
+    Ch = max(xt);
+    
+    % run SVM analyses
+    for i = 1:numel(SVMs)
+
+        % Option 1: correct data for covariates
+        if i == 1 || i == 3, Yi = Yr{h};                    end;
+        % Option 2: use covariates as features
+        if i == 2,           Yi =[Ys{h}, Xs{h}(:,1:end-1)]; end;
+
+        % perform cross-validation
+        if i == p, tic; end;
+        xp = zeros(size(xt));
+        for g = 1:size(CV{h},2)
+            % get training and test set
+            i1  = find(CV{h}(:,g)==1);
+            i2  = find(CV{h}(:,g)==2);
+            Y1g = Yi(i1,:);
+            Y2g = Yi(i2,:);
+            x1g = xt(i1);
+            x2g = xt(i2);
+            opt = sprintf('-s 0 -t 0 -c %s -q', num2str(C));
+            % Option 3: weight samples by class size
+            if i == 3
+                for j = 1:Ch
+                    opt = sprintf('%s -w%d %0.3f', opt, j, (1/Ch)*(numel(i1)/sum(x1g==j)));
+                end;
+            end;
+            % train and test using SVC
+            svm1   = svmtrain(x1g, Y1g, opt);
+            xp(i2) = svmpredict(x2g, Y2g, svm1, '-q');
+        end;
+
+        % store SVM results
+        SVC(h,i).pred.xt  = xt;
+        SVC(h,i).pred.xp  = xp;
+        SVC(h,i).perf.CA  = mean(xp==xt);
+        SVC(h,i).perf.CAs = zeros(Ch,1);
+        SVC(h,i).perf.CM  = zeros(Ch,Ch);
+        for j = 1:Ch
+            SVC(h,i).perf.CAs(j)  = mean(xp(xt==j)==j);
+            SVC(h,i).perf.CM(:,j) = ...
+                mean( repmat(xp(xt==j),[1,Ch])==repmat([1:Ch],[sum(xt==j),1]) )';
+        end;
+        SVC(h,i).perf.BA  = mean(SVC(h,i).perf.CAs);
+        if i == p, tB = tB + toc; end;
+        
+    end;
+    
+end;
+clear Ch xt xp Yi i1 i2 Y1g Y2g x1g x2g opt svm1
+
 % collect balanced accuracies
 BA = zeros(2,3);
 for h = 1:3
     BA(1,h) = MBC(h).perf.BA;
-    BA(2,h) = SVC(h).perf.BA;
+    BA(2,h) = SVC(h,p).perf.BA;
 end;
 
 % store analysis time
@@ -106,8 +167,8 @@ for h = 2 % 1:numel(labs)
     nC(h) = max(X(:,h));
     for g = 1:2
         subplot(3,4,(h-1)*4+(2+g));
-        if g == 1, CM = MBC(h).perf.CM; end;
-        if g == 2, CM = SVC(h).perf.CM; end;
+        if g == 1, CM = MBC(h).perf.CM;   end;
+        if g == 2, CM = SVC(h,p).perf.CM; end;
         imagesc(CM);
         caxis([0, 1]);
         colormap(cmap3);
@@ -176,8 +237,8 @@ for h = [1,3] % 1:numel(labs)
     nC(h) = max(X(:,h));
     for g = 1:2
         subplot(3,4,(h-1)*4+(2+g));
-        if g == 1, CM = MBC(h).perf.CM; end;
-        if g == 2, CM = SVC(h).perf.CM; end;
+        if g == 1, CM = MBC(h).perf.CM;   end;
+        if g == 2, CM = SVC(h,p).perf.CM; end;
         imagesc(CM);
         caxis([0, 1]);
         colormap(cmap2);
@@ -189,7 +250,10 @@ for h = [1,3] % 1:numel(labs)
         ylabel('predicted class', 'FontSize', 12);
         if h == 1
             if g == 1, title('MBC with covariates', 'FontSize', 16); end;
-            if g == 2, title('SVC with regression', 'FontSize', 16);    end;
+            if g == 2
+                if p ~= 2, title('SVC with regression', 'FontSize', 16);
+                else,      title('SVC with features',   'FontSize', 16); end;
+            end;
         end;
         for c1 = 1:nC(h)
             for c2 = 1:nC(h)
